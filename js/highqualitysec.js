20600c401e19adeb57920425abd5da882982b55e
qym <100487501+kivrola@users.noreply.github.com>
2023-03-04 22:29:45 +0300

    Update highqualitysec.js

--- js/highqualitysec.js
+++ js/highqualitysec.js
@@ -1,25 +1,27 @@
-var skidlink = 'https://resources.infosecinstitute.com/topic/25-ways-to-become-the-ultimate-script-kiddie/'
+// set the URL for redirection
+var skidlink = 'https://resources.infosecinstitute.com/topic/25-ways-to-become-the-ultimate-script-kiddie/';
 
-
-//RIGHTCLICK BLOCKING
+// RIGHT-CLICK BLOCKING
 document.addEventListener('contextmenu', function(event) {
     event.preventDefault();
 });
 
-
-//SAVING DETECTIONS
-document.onkeydown = function(event) {
+// SAVING DETECTIONS
+document.addEventListener('keydown', function(event) {
     if (event.ctrlKey && (event.keyCode === 83 || event.keyCode === 115)) {
         event.preventDefault();
         window.alert("Control S detected!🤓");
         window.location.href = skidlink;
     }
-};
+});
 
+// DEVTOOLS DETECTIONS
+// load devtools-detect library script
+var devtoolsScript = document.createElement('script');
+devtoolsScript.setAttribute('src', 'https://unpkg.com/devtools-detect');
+document.head.appendChild(devtoolsScript);
 
-//DEVTOOLS DETECTIONS
-<script src="https://unpkg.com/devtools-detect"></script>
-
+// add devtools detection using devtools-detect
 devtools.addListener((isOpen) => {
   if (isOpen) {
     window.alert("DevTools detected!🤓");
@@ -27,6 +29,7 @@ devtools.addListener((isOpen) => {
   }
 });
 
+// add F12 key detection for DevTools
 document.addEventListener('keydown', function(event) {
   if (event.keyCode == 123) { 
     event.preventDefault();
@@ -35,12 +38,12 @@ document.addEventListener('keydown', function(event) {
   }
 });
 
+// add debugger detection
 var minimalUserResponseInMiliseconds = 100;
 var before = new Date().getTime();
 debugger;
 var after = new Date().getTime();
 if (after - before > minimalUserResponseInMiliseconds) {
-  event.preventDefault();
-  window.alert("DevTools detected!🤓");
+  window.alert("Debugger detected!🤓");
   window.location.href = skidlink; 
 }
