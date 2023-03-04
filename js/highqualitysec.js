405be14cf6f7d709242daeff5ae4b8c80c175144
qym <100487501+kivrola@users.noreply.github.com>
2023-03-04 22:17:08 +0300

    Update highqualitysec.js

--- js/highqualitysec.js
+++ js/highqualitysec.js
@@ -1,3 +1,5 @@
+var skidlink = 'https://resources.infosecinstitute.com/topic/25-ways-to-become-the-ultimate-script-kiddie/'
+
 
 document.addEventListener('contextmenu', function(event) {
     event.preventDefault();
@@ -7,15 +9,15 @@ document.onkeydown = function(event) {
     if (event.ctrlKey && (event.keyCode === 83 || event.keyCode === 115)) {
         event.preventDefault();
         window.alert("Control S detected!🤓");
-        window.location.href = "https://resources.infosecinstitute.com/topic/25-ways-to-become-the-ultimate-script-kiddie/";
+        window.location.href = skidlink;
     }
 };
 
 let devtoolsOpen = false;
 
 const checkDevTools = () => {
-  if (devtoolsOpen || (window.outerWidth - window.innerWidth > 100) || (window.outerHeight - window.innerHeight > 100)) {
-    window.location.href = "https://resources.infosecinstitute.com/topic/25-ways-to-become-the-ultimate-script-kiddie/";
+  if (devtoolsOpen || window.devtools.isOpen()) {
+    window.location.href = skidlink
     alert("Developer tools detected!🤓");
   }
 }
