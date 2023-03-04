4d2795927ee533afe6ff14bcebf8a9b35c75620a
qym <100487501+kivrola@users.noreply.github.com>
2023-03-04 18:22:33 +0300

    Update devtoolsaregay.js

--- js/devtoolsaregay.js
+++ js/devtoolsaregay.js
@@ -1,8 +1,16 @@
 // Check if devtools are open
 if (typeof window !== 'undefined') {
-  window.addEventListener('devtoolschange', event => {
+  const element = new Image();
+  let devtoolsOpen = false;
+  element.__defineGetter__('id', function() {
+    devtoolsOpen = true;
     // Display popup message and redirect
     alert("Detected!");
     window.location.href = "https://resources.infosecinstitute.com/topic/25-ways-to-become-the-ultimate-script-kiddie/";
   });
+  setInterval(function() {
+    devtoolsOpen = false;
+    console.log(element);
+    console.clear();
+  }, 1000);
 }
