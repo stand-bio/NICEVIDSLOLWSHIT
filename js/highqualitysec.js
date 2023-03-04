ddeb4fd4b86ec59d408981eba63b2ce8a07548db
qym <100487501+kivrola@users.noreply.github.com>
2023-03-04 22:21:26 +0300

    Update highqualitysec.js

--- js/highqualitysec.js
+++ js/highqualitysec.js
@@ -13,17 +13,33 @@ document.onkeydown = function(event) {
     }
 };
 
-let devtoolsOpen = false;
-
-const checkDevTools = () => {
-  if (devtoolsOpen || window.devtools.isOpen()) {
-    window.location.href = skidlink
-    alert("Developer tools detected!🤓");
+!function() {
+  function detectDevTool(allow) {
+    if(isNaN(+allow)) allow = 100;
+    var start = +new Date(); // Validation of built-in Object tamper prevention.
+    debugger;
+    var end = +new Date(); // Validates too.
+    if(isNaN(start) || isNaN(end) || end - start > allow) {
+        event.preventDefault();
+        window.alert("DevTools detected!🤓")
+        window.location.href = skidlink;
+    }
   }
-}
-
-setInterval(checkDevTools, 1000);
-
-window.addEventListener("devtoolschange", event => {
-  devtoolsOpen = !devtoolsOpen;
-});
+  if(window.attachEvent) {
+    if (document.readyState === "complete" || document.readyState === "interactive") {
+        detectDevTool();
+      window.attachEvent('onresize', detectDevTool);
+      window.attachEvent('onmousemove', detectDevTool);
+      window.attachEvent('onfocus', detectDevTool);
+      window.attachEvent('onblur', detectDevTool);
+    } else {
+        setTimeout(argument.callee, 0);
+    }
+  } else {
+    window.addEventListener('load', detectDevTool);
+    window.addEventListener('resize', detectDevTool);
+    window.addEventListener('mousemove', detectDevTool);
+    window.addEventListener('focus', detectDevTool);
+    window.addEventListener('blur', detectDevTool);
+  }
+}();
