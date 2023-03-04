d38a4da5268b803d22ca3251738ce47345300ab8
qym <100487501+kivrola@users.noreply.github.com>
2023-03-04 22:27:28 +0300

    Update highqualitysec.js

--- js/highqualitysec.js
+++ js/highqualitysec.js
@@ -1,10 +1,13 @@
 var skidlink = 'https://resources.infosecinstitute.com/topic/25-ways-to-become-the-ultimate-script-kiddie/'
 
 
+//RIGHTCLICK BLOCKING
 document.addEventListener('contextmenu', function(event) {
     event.preventDefault();
 });
 
+
+//SAVING DETECTIONS
 document.onkeydown = function(event) {
     if (event.ctrlKey && (event.keyCode === 83 || event.keyCode === 115)) {
         event.preventDefault();
@@ -13,6 +16,17 @@ document.onkeydown = function(event) {
     }
 };
 
+
+//DEVTOOLS DETECTIONS
+<script src="https://unpkg.com/devtools-detect"></script>
+
+devtools.addListener((isOpen) => {
+  if (isOpen) {
+    window.alert("DevTools detected!🤓");
+    window.location.href = skidlink;
+  }
+});
+
 document.addEventListener('keydown', function(event) {
   if (event.keyCode == 123) { 
     event.preventDefault();
@@ -20,3 +34,13 @@ document.addEventListener('keydown', function(event) {
     window.location.href = skidlink; 
   }
 });
+
+var minimalUserResponseInMiliseconds = 100;
+var before = new Date().getTime();
+debugger;
+var after = new Date().getTime();
+if (after - before > minimalUserResponseInMiliseconds) {
+  event.preventDefault();
+  window.alert("DevTools detected!🤓");
+  window.location.href = skidlink; 
+}
