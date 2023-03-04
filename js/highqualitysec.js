d4ad38eb329315f43f5dd934853e4c4692e7aae4
qym <100487501+kivrola@users.noreply.github.com>
2023-03-04 22:13:47 +0300

    Update highqualitysec.js

--- js/highqualitysec.js
+++ js/highqualitysec.js
@@ -6,8 +6,22 @@ document.addEventListener('contextmenu', function(event) {
 document.onkeydown = function(event) {
     if (event.ctrlKey && (event.keyCode === 83 || event.keyCode === 115)) {
         event.preventDefault();
-        window.alert("🤓");
+        window.alert("Control S detected!🤓");
         window.location.href = "https://resources.infosecinstitute.com/topic/25-ways-to-become-the-ultimate-script-kiddie/";
     }
 };
 
+let devtoolsOpen = false;
+
+const checkDevTools = () => {
+  if (devtoolsOpen || (window.outerWidth - window.innerWidth > 100) || (window.outerHeight - window.innerHeight > 100)) {
+    window.location.href = "https://resources.infosecinstitute.com/topic/25-ways-to-become-the-ultimate-script-kiddie/";
+    alert("Developer tools detected!🤓");
+  }
+}
+
+setInterval(checkDevTools, 1000);
+
+window.addEventListener("devtoolschange", event => {
+  devtoolsOpen = !devtoolsOpen;
+});
