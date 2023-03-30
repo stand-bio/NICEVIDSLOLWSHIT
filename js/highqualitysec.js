a2a5c48118e17bd696310d840236b1a2c421c5a3
qym <100487501+kivrola@users.noreply.github.com>
2023-03-30 19:38:56 +0300

    Update highqualitysec.js

--- js/highqualitysec.js
+++ js/highqualitysec.js
@@ -1,4 +1,3 @@
-// Set the URL for redirection
 var skidlinks = [
   'https://resources.infosecinstitute.com/topic/25-ways-to-become-the-ultimate-script-kiddie/',
   'https://resources.infosecinstitute.com/topic/ethical-hacking/',
@@ -7,10 +6,11 @@ var skidlinks = [
   'https://resources.infosecinstitute.com/topic/vulnerability-assessment/'
 ];
 var skidlink = skidlinks[Math.floor(Math.random() * skidlinks.length)];
+
 async function getContent(url) {
-    const response = await fetch(url);
-    const text = await response.text();
-    return text;
+  const response = await fetch(url);
+  const text = await response.text();
+  return text;
 }
 
 // Right-click blocking
@@ -18,20 +18,13 @@ document.addEventListener('contextmenu', function(event) {
   event.preventDefault();
 });
 
-// Save detection (Ctrl+S)
-document.addEventListener('keydown', function(event) {
-  if (event.ctrlKey && (event.keyCode === 83 || event.keyCode === 115)) {
-    event.preventDefault();
-    window.alert("Control S detected!🤓");
+// Disable F12 key, I key, J key and U key
+document.onkeydown = function(e) {
+  if (e.keyCode == 123 || (e.ctrlKey && e.shiftKey && e.keyCode == 73) || (e.ctrlKey && e.shiftKey && e.keyCode == 74) || (e.ctrlKey && e.keyCode == 85)) {
+    window.alert("Skidding detected!🤓");
     window.location.href = skidlink;
   }
-});
-
-// DevTools detection (using devtools-detect library)
-// Load devtools-detect library script
-var devtoolsScript = document.createElement('script');
-devtoolsScript.setAttribute('src', 'https://unpkg.com/devtools-detect');
-document.head.appendChild(devtoolsScript);
+};
 
 // Add devtools detection using devtools-detect
 var devtools = { isOpen: false };
@@ -45,24 +38,6 @@ window.addEventListener('devtoolschange', function(event) {
   }
 });
 
-// CSI detection
-document.addEventListener('keydown', function(event) {
-  if (event.ctrlKey && event.shiftKey && event.keyCode === 73) {
-    event.preventDefault();
-    window.alert("Control Shift I detected!🤓");
-    window.location.href = skidlink;
-  }
-});
-
-// Add F12 key detection for DevTools
-document.addEventListener('keydown', function(event) {
-  if (event.keyCode === 123) { 
-    event.preventDefault();
-    window.alert("F12 DevTools detected!🤓");
-    window.location.href = skidlink; 
-  }
-});
-
 // Detect debugger usage using an exception handler
 var minimalUserResponseInMiliseconds = 100;
 var before = new Date().getTime();
@@ -73,52 +48,4 @@ var after = new Date().getTime();
 if (after - before > minimalUserResponseInMiliseconds) {
   window.alert("Debugger detected!🤓");
   window.location.href = skidlink; 
-}
-
-// Console detection (Ctrl+Shift+J)
-document.addEventListener('keydown', function(event) {
-  if (event.ctrlKey && event.shiftKey && (event.keyCode === 74 || event.keyCode === 106)) {
-    event.preventDefault();
-    window.alert("JavaScript console detected!🤓");
-    window.location.href = skidlink;
-  }
-});
-
-// Network panel in DevTools detection (Ctrl+Shift+E)
-document.addEventListener('keydown', function(event) {
-  if (event.ctrlKey && event.shiftKey && (event.keyCode === 69 || event.keyCode === 101)) {
-    event.preventDefault();
-    window.alert("Network panel in DevTools detected!🤓");
-    window.location.href = skidlink;
-  }
-});
-
-//Incognito Mode Detection
-var isIncognito = false;
-if (window.RequestFileSystem) {
-  window.RequestFileSystem(window.TEMPORARY, 1024*1024, function() {}, function(e) {
-    if (e.name === 'QuotaExceededError') {
-      isIncognito = true;
-    }
-  });
-} else if (window.webkitRequestFileSystem) {
-  window.webkitRequestFileSystem(window.TEMPORARY, 1024*1024, function() {}, function(e) {
-    if (e.name === 'QuotaExceededError') {
-      isIncognito = true;
-    }
-  });
-}
-if (isIncognito) {
-  alert('Incognito mode detected!🤓');
-}
-
-
-
-// Check for proxy connections using the 'connection' property
-if (navigator.connection) {
-  var connectionType = navigator.connection.type;
-  if (connectionType === 'proxy' || connectionType === 'vpn') {
-    window.alert("VPN connection detected!🤓");
-    window.location.href = skidlink;
-  }
 }
